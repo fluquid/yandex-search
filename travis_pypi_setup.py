2738d0be5db2c7e245426f96c4e6287fac1c457e
Johannes Ahlmann <johannes@scrapinghub.com>
2017-06-02 22:54:36 +0200

    minimal commit to test travis-ci

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -32,7 +32,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'codinguncut/yandex-search'
+GITHUB_REPO = 'fluquid/yandex-search'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
